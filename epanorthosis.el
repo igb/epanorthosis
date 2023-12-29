44f26af69e039c04a0ca8a03fcd8b76498aafed1
Ian Brown <ibrown@netflix.com>
2023-12-29 14:44:21 -0800

    prompting for replacement

--- epanorthosis.el
+++ epanorthosis.el
@@ -2,25 +2,33 @@
   "Indicate a replacement of a word or text using '^H' characters."
   
   (interactive)
+  
 
   (if mark-active
       (let (
 	    (selection (buffer-substring-no-properties (region-beginning) (region-end))))
 	(if (= (length selection) 0)
 	    (print "nothing selected")
-	  (setq slength  (length selection))
-	  (setq count 0)
-          (setq acc "")
-	 
 
-	  (while (< count slength)
-	    (setq acc (concat acc "^H"))
-	    (setq count (1+ count)))
-	  (print acc)
-	 
+	  (setq replacement (read-string "'replace' with: "))
+
+	  (if (= (length replacement) 0)
+	      (print "no replacement text?")
+	    (setq slength  (length selection))
+	    (setq count 0)
+            (setq acc "")
+	    
+	    
+	    (while (< count slength)
+	      (setq acc (concat acc "^H"))
+	      (setq count (1+ count)))
+
+	    (print (concat selection acc " " replacement))
+	    
+	    )
+	  )
 	)
-    )
-    (print "nothing selected")
-    
+	(print "nothing selected")
+     
     )
   )
